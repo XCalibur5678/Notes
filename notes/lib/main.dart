bd22e7bad1cdfa730e84458fb0b9ce8e750a5cea
saugnik <148228363+saugnik@users.noreply.github.com>
2024-06-23 21:22:07 +0530

    Changes

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -1,118 +1,210 @@
 import 'package:flutter/material.dart';
-
-// ... (Note class definition - see step 2)
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class Note {
-  String title;
-  String content;
-
-  Note({required this.title, required this.content});
-}
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Notes',
-      home: NoteListScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => NoteProvider(),
+      child: MaterialApp(
+        title: 'Notepad App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: NoteListScreen(),
+      ),
     );
   }
 }
 
-class NoteListScreen extends StatefulWidget {
-  @override
-  _NoteListScreenState createState() => _NoteListScreenState();
+class Note {
+  String id;
+  String title;
+  String content;
+  DateTime date;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.date,
+  });
 }
 
-class _NoteListScreenState extends State<NoteListScreen> {
-  List<Note> notes = []; // Our in-memory note list
+class NoteProvider with ChangeNotifier {
+  final List<Note> _notes = [];
 
-  // ... (Methods for adding, editing, deleting notes)
+  List<Note> get notes => _notes;
 
+  void addNote(Note note) {
+    _notes.add(note);
+    notifyListeners();
+  }
+
+  void updateNote(Note note) {
+    int index = _notes.indexWhere((n) => n.id == note.id);
+    if (index != -1) {
+      _notes[index] = note;
+      notifyListeners();
+    }
+  }
+
+  void deleteNote(String id) {
+    _notes.removeWhere((note) => note.id == id);
+    notifyListeners();
+  }
+}
+
+class NoteListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final notes = Provider.of<NoteProvider>(context).notes;
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Notes')),
+      appBar: AppBar(
+        title: Text('Notepad App'),
+      ),
       body: ListView.builder(
         itemCount: notes.length,
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(notes[index].title),
             subtitle: Text(notes[index].content),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                Provider.of<NoteProvider>(context, listen: false)
+                    .deleteNote(notes[index].id);
+              },
+            ),
             onTap: () {
-              // ... (Navigate to note editing screen)
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NoteEditScreen(note: notes[index]),
+                ),
+              );
             },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // ... (Navigate to note adding screen)
-        },
         child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => NoteEditScreen(),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
 class NoteEditScreen extends StatefulWidget {
-  final Note note;
+  final Note? note;
 
-  NoteEditScreen({required this.note});
+  NoteEditScreen({this.note});
 
   @override
   _NoteEditScreenState createState() => _NoteEditScreenState();
 }
 
 class _NoteEditScreenState extends State<NoteEditScreen> {
-  late TextEditingController titleController;
-  late TextEditingController contentController;
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _content = '';
 
   @override
   void initState() {
     super.initState();
-    titleController = TextEditingController(text: widget.note.title);
-    contentController = TextEditingController(text: widget.note.content);
+    if (widget.note != null) {
+      _title = widget.note!.title;
+      _content = widget.note!.content;
+    }
   }
 
-  @override
-  void dispose() {
-    titleController.dispose();
-    contentController.dispose();
-    super.dispose();
+  void _saveNote() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      if (widget.note == null) {
+        final newNote = Note(
+          id: Uuid().v4(),
+          title: _title,
+          content: _content,
+          date: DateTime.now(),
+        );
+        Provider.of<NoteProvider>(context, listen: false).addNote(newNote);
+      } else {
+        final updatedNote = Note(
+          id: widget.note!.id,
+          title: _title,
+          content: _content,
+          date: DateTime.now(),
+        );
+        Provider.of<NoteProvider>(context, listen: false).updateNote(updatedNote);
+      }
+
+      Navigator.pop(context);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit Note')),
+      appBar: AppBar(
+        title: Text(widget.note == null ? 'New Note' : 'Edit Note'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: InputDecoration(hintText: 'Title'),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: contentController,
-              decoration: InputDecoration(hintText: 'Content'),
-              maxLines: null,
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // ... (Save note and navigate back)
-              },
-              child: Text('Save'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                initialValue: _title,
+                decoration: InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _title = value!;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                initialValue: _content,
+                decoration: InputDecoration(labelText: 'Content'),
+                maxLines: 10,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter content';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _content = value!;
+                },
+              ),
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: _saveNote,
       ),
     );
   }
